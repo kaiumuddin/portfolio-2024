7c4da3934dafbec01a6e28230fdf768be4fa2782
kaiumuddin <kaium.uddin2909@gmail.com>
2023-12-31 15:58:48 +0600

    Navbar done

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+// components
+import ThemeToggler from "./ThemeToggler";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* nav */}
+            <Nav
+              containerStyles="hidden xl:flex gap-x-8 items-center transition-all"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
+            <ThemeToggler />
+            {/* mobile nav */}
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
