f39be0f7cebc2f07748856af58310ea0f1f84b94
kaiumuddin <kaium.uddin2909@gmail.com>
2024-01-02 19:49:56 +0600

    Service section designed and added some services

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,14 @@
-
 // components
-import About from '@/components/About'
-import Hero from '@/components/Hero'
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
       <About />
+      <Services />
     </main>
-  )
+  );
 }
